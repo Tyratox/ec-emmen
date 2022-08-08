a50f586ecf700a4f2a2efa3b82cfe8dd1fa3b3a8
Nico Hauser <me@tyratox.ch>
2022-08-08 16:01:11 +0200

    Support multiple gallery links

--- template-parts/blocks/gallery-link/gallery-link.php
+++ template-parts/blocks/gallery-link/gallery-link.php
@@ -1,45 +1,45 @@
 <?php
 	
-	$page_id = get_field("page");
+	$page_ids = get_field("pages");
 	
-	if(is_array($page_id) && count($page_id) == 1){
-		$page_id = $page_id[0];
-	}else{
-		echo "Ungültige Seite verlinkt";
+	if(!is_array($page_ids) || count($page_ids) == 0){
+		echo "Keine Seite verlinkt";
 		return;
 	}
 	
-	// get all blocks of the page
-	$blocks = parse_blocks(get_the_content(null, false, $page_id));
-	
-	// filter for galleries
-	$galleries = array_values(array_filter($blocks, function($block){
-		return $block["blockName"] == "acf/gallery";
-	}));
-	
-	// ensure there is only a single one
-	if(count($galleries) != 1){
-		echo "Die verlinkte Seite muss genau eine einzelne Gallerie enthalten";
-		return;
+	foreach($page_ids as $page_id){
+		// get all blocks of the page
+		$blocks = parse_blocks(get_the_content(null, false, $page_id));
+		
+		// filter for galleries
+		$galleries = array_values(array_filter($blocks, function($block){
+			return $block["blockName"] == "acf/gallery";
+		}));
+		
+		// ensure there is only a single one
+		if(count($galleries) != 1){
+			echo "Die verlinkte Seite muss genau eine einzelne Gallerie enthalten";
+			return;
+		}
+		
+		$gallery = $galleries[0];
+		
+		$attachment_ids = $gallery["attrs"]["data"]["gallery"];
+		
+		
+		$url = "/wp-json/ec-emmen/download/" . implode(",", $attachment_ids);
+		
+		echo "<div class='border-b-2 border-red pb-4 my-4 flex items-center'>";
+			
+			echo '<a download class="hover:text-red flex items-center" href="' . $url . '" title="Alle Bilder herunterladen">';
+				echo '<i class="fas fa-cloud-download-alt text-3xl w-10 h-auto mr-2"></i>';
+			echo '</a>';
+			
+			echo '<a class="hover:text-red flex" href="' . get_permalink($page_id) . '" title="Zur Gallerie">';
+				echo get_the_title($page_id);
+			echo '</a>';
+			
+		echo "</div>";	
 	}
-	
-	$gallery = $galleries[0];
-	
-	$attachment_ids = $gallery["attrs"]["data"]["gallery"];
-	
-	
-	$url = "/wp-json/ec-emmen/download/" . implode(",", $attachment_ids);
-	
-	echo "<div class='border-b-2 border-red pb-4 my-4 flex items-center'>";
-		
-		echo '<a download class="hover:text-red flex items-center" href="' . $url . '" title="Alle Bilder herunterladen">';
-			echo '<i class="fas fa-cloud-download-alt text-3xl w-10 h-auto mr-2"></i>';
-		echo '</a>';
-		
-		echo '<a class="hover:text-red flex" href="' . get_permalink($page_id) . '" title="Zur Gallerie">';
-			echo get_the_title($page_id);
-		echo '</a>';
-		
-	echo "</div>";
 	
 ?>
